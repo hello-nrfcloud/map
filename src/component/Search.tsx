4ac2858d3a39b804cbbe6b0218c500628c19eec6
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-23 12:41:39 +0100

    feat: implement simple search

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -6,12 +6,12 @@ import { SidebarContent } from './Sidebar.jsx'
 import { linkToDevice, linkToHome } from '../util/link.js'
 import { Device as DeviceIcon } from '../icons/Device.js'
 import { useNavigation } from '../context/Navigation.jsx'
-import { instanceTs } from '../util/instanceTs.js'
 import { RelativeTime } from './RelativeTime.jsx'
 
 enum SearchTermType {
 	Id = 'id',
 	Model = 'model',
+	Any = '*',
 }
 type SearchTerm = {
 	type: SearchTermType
@@ -23,16 +23,18 @@ const isSearchTermType = (term: unknown): term is SearchTermType =>
 	typeof term === 'string' && allowedTypes.includes((term ?? '') as any)
 
 const Search = () => {
-	const [searchTerms, setSearchTerms] = createSignal<SearchTerm[]>([
-		{ type: SearchTermType.Id, term: 'pentacid-coxalgia-backheel' },
-		{ type: SearchTermType.Model, term: 'PCA20035+solar' },
-	])
+	const [searchTerms, setSearchTerms] = createSignal<SearchTerm[]>([])
 	let input!: HTMLInputElement
 
 	const addSearchTerm = () => {
 		if (input.value.length > 0) {
 			const [type, term] = input.value.split(':')
-			if (isSearchTermType(type) && term !== undefined) {
+			if (type !== undefined && term === undefined) {
+				setSearchTerms((prev) => [
+					...prev,
+					{ type: SearchTermType.Any, term: type },
+				])
+			} else if (isSearchTermType(type) && term !== undefined) {
 				setSearchTerms((prev) => [...prev, { type, term } as SearchTerm])
 			}
 		}
@@ -48,17 +50,21 @@ const Search = () => {
 						e.stopPropagation()
 					}}
 				>
-					<input
-						type="search"
-						placeholder='e.g. "id:<device id>"'
-						ref={input}
-						onKeyUp={(e) => {
-							if (e.key === 'Enter') addSearchTerm()
-						}}
-					/>
-					<button type="button">
-						<AddToSearch size={20} />
-					</button>
+					<label for="search">Enter your search term:</label>
+					<div class="input-group">
+						<input
+							id="search"
+							type="search"
+							placeholder='e.g. "id:<device id>"'
+							ref={input}
+							onKeyUp={(e) => {
+								if (e.key === 'Enter') addSearchTerm()
+							}}
+						/>
+						<button type="button">
+							<AddToSearch size={20} />
+						</button>
+					</div>
 				</form>
 				<Show when={searchTerms().length > 0}>
 					<div class="terms">
@@ -79,7 +85,10 @@ const Search = () => {
 					</div>
 				</Show>
 			</div>
-			<Show when={searchTerms().length > 0}>
+			<Show
+				when={searchTerms().length > 0}
+				fallback={<MostRecentDevicesList />}
+			>
 				<SearchResult terms={searchTerms()} />
 			</Show>
 		</>
@@ -108,69 +117,74 @@ export const Sidebar = () => {
 }
 
 const SearchResult = ({ terms }: { terms: SearchTerm[] }) => {
-	const [results, setResults] = createSignal<
-		{ device: Device; lastUpdate: Date | undefined }[]
-	>([])
+	const [results, setResults] = createSignal<Device[]>([])
 	const devices = useDevices()
 
-	createEffect(() =>
-		setResults(
-			devices()
-				.filter(matches(terms))
-				.map((device) => ({ device, lastUpdate: updateTs(device) })),
-		),
-	)
+	createEffect(() => setResults(devices().filter(matches(terms))))
 
 	return (
 		<section class="results">
 			<header>
 				<h2>Results</h2>
 			</header>
-			<For
-				each={results().sort(mostRecentUpdateFirst)}
-				fallback={<p>No matching devices found.</p>}
-			>
-				{({ device, lastUpdate }) => {
-					return (
-						<div class="result boxed">
-							<DeviceIcon class="icon" />
-							<span>
-								<a href={linkToDevice(device.id)}>
-									<code>{device.id}</code>
-								</a>
-								<br />
-								<small>{device.model}</small>
-								<br />
-								<Show
-									when={lastUpdate !== undefined}
-									fallback={<small>Never updated.</small>}
-								>
-									<small>
-										<RelativeTime time={lastUpdate!} />
-									</small>
-								</Show>
-							</span>
-						</div>
-					)
-				}}
+			<For each={results()} fallback={<p>No matching devices found.</p>}>
+				{(device) => <DeviceCard device={device} />}
 			</For>
 		</section>
 	)
 }
 
-const matches = (terms: SearchTerm[]) => (/*device: Device*/) =>
-	terms.reduce((matches) => matches, true)
+const MostRecentDevicesList = () => {
+	const devices = useDevices()
 
-const mostRecentUpdateFirst = (
-	{ lastUpdate: u1 }: { lastUpdate: Date | undefined },
-	{ lastUpdate: u2 }: { lastUpdate: Date | undefined },
-): number => {
-	const d1Update = u1?.getTime() ?? Number.MIN_SAFE_INTEGER
-	const d2Update = u2?.getTime() ?? Number.MIN_SAFE_INTEGER
-	return d2Update - d1Update
+	return (
+		<section class="results">
+			<header>
+				<h2>Recently updated devices</h2>
+			</header>
+			<For
+				each={devices().slice(0, 10)}
+				fallback={<p>No matching devices found.</p>}
+			>
+				{(device) => <DeviceCard device={device} />}
+			</For>
+		</section>
+	)
 }
 
-const updateTs = (device: Device): Date | undefined =>
-	(device.state ?? []).map(instanceTs).sort(desc)[0]
+const DeviceCard = ({ device }: { device: Device }) => (
+	<div class="result boxed">
+		<DeviceIcon class="icon" />
+		<span>
+			<a href={linkToDevice(device.id)}>
+				<code>{device.id}</code>
+			</a>
+			<br />
+			<small>{device.model}</small>
+			<br />
+			<Show
+				when={device.lastUpdate !== undefined}
+				fallback={<small>Never updated.</small>}
+			>
+				<small>
+					<RelativeTime time={device.lastUpdate!} />
+				</small>
+			</Show>
+		</span>
+	</div>
+)
 
-const desc = (d1: Date, d2: Date) => d2.getTime() - d1.getTime()
+const matches = (terms: SearchTerm[]) => (device: Device) =>
+	terms.reduce((matches, term) => {
+		if (matches === false) return false
+		return termMatchesDevice(term, device)
+	}, true)
+
+const termMatchesDevice = (term: SearchTerm, device: Device) => {
+	const tokens = []
+	if (term.type === SearchTermType.Id || term.type === SearchTermType.Any)
+		tokens.push(device.id)
+	if (term.type === SearchTermType.Model || term.type === SearchTermType.Any)
+		tokens.push(device.model)
+	return tokens.join(' ').includes(term.term)
+}
--- src/context/Devices.tsx
+++ src/context/Devices.tsx
@@ -3,8 +3,11 @@ import { createResource, createContext, useContext } from 'solid-js'
 import { useParameters } from './Parameters.js'
 import { Devices, PublicDevice } from '@hello.nrfcloud.com/proto/hello/map'
 import { type Static } from '@sinclair/typebox'
+import { instanceTs } from '../util/instanceTs.js'
 
-export type Device = Static<typeof PublicDevice>
+export type Device = Static<typeof PublicDevice> & {
+	lastUpdate: Date | undefined
+}
 
 export const fetchDevices =
 	(url: URL) => async (): Promise<Static<typeof Devices>> => {
@@ -32,10 +35,12 @@ export const DevicesProvider = (props: ParentProps) => {
 
 	return (
 		<DevicesContext.Provider
-			value={() => [
-				...(devicesResource()?.devices ?? []),
-				...(thingyWorldDevicesResource()?.devices ?? []),
-			]}
+			value={() =>
+				[
+					...(devicesResource()?.devices ?? []).map(addLastUpdated),
+					...(thingyWorldDevicesResource()?.devices ?? []).map(addLastUpdated),
+				].sort(byLastUpdated)
+			}
 		>
 			{props.children}
 		</DevicesContext.Provider>
@@ -50,3 +55,19 @@ export const byId =
 	(search: string) =>
 	({ id }: { id: string }) =>
 		id === search
+
+const addLastUpdated = (device: Static<typeof PublicDevice>): Device => ({
+	...device,
+	lastUpdate: (device.state ?? []).map(instanceTs).sort(desc)[0],
+})
+
+const desc = (d1: Date, d2: Date) => d2.getTime() - d1.getTime()
+
+const byLastUpdated = (
+	{ lastUpdate: u1 }: { lastUpdate: Date | undefined },
+	{ lastUpdate: u2 }: { lastUpdate: Date | undefined },
+): number => {
+	const d1Update = u1?.getTime() ?? Number.MIN_SAFE_INTEGER
+	const d2Update = u2?.getTime() ?? Number.MIN_SAFE_INTEGER
+	return d2Update - d1Update
+}
