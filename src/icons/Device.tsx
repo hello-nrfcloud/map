978015bbcd55fa379d029a43e06dcc8b2288fd7f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-28 15:22:51 +0100

    fix: smaller UI improvements

--- src/icons/Device.tsx
+++ src/icons/Device.tsx
@@ -1,12 +1,10 @@
 export const Device = ({ class: className }: { class?: string }) => (
 	<svg
 		version="1.1"
-		id="Layer_1"
 		xmlns="http://www.w3.org/2000/svg"
 		x="0px"
 		y="0px"
 		viewBox="0 0 1024 1024"
-		style="enable-background:new 0 0 1024 1024;"
 		class={className ?? ''}
 	>
 		<g>
