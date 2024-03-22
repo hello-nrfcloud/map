8fa69eaafd471bfef2ee2069f1c269c9ff9af4be
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-22 12:09:29 +0100

    test(e2e): allow to change backend state through API

---
+++ e2e-tests/mock-backend.ts
@@ -0,0 +1,63 @@
+import type { Connect } from 'vite'
+import type { Parameters } from '../src/context/Parameters.tsx'
+import { Context } from '@hello.nrfcloud.com/proto/hello'
+import type { IncomingMessage, ServerResponse } from 'http'
+
+export const mockBackend = ({
+	registry,
+}: {
+	registry: Parameters
+}): Record<string, Connect.SimpleHandleFunction> => {
+	let release = '0.0.0-development'
+	return {
+		'/e2e/registry.json': (req, res) =>
+			sendJSON(res, generateRegistryResponse(registry)),
+		'/e2e/api/devices': (req, res) =>
+			sendJSON(res, {
+				'@context': Context.map.devices,
+				devices: [],
+			}),
+		'/e2e/api/thingyWorldShadows': (req, res) =>
+			sendJSON(res, {
+				'@context': Context.map.devices,
+				devices: [],
+			}),
+		'/map/.well-known/release': (req, res) => {
+			res.setHeader('Content-type', 'text/plain; charset=utf-8')
+			res.write(release)
+			res.end()
+		},
+		'/api/release': (req, res) => {
+			if (req.method === 'PUT')
+				req.on('data', (d) => {
+					release = d.toString().trim()
+				})
+			res.statusCode = 202
+			res.end()
+		},
+	}
+}
+
+const generateRegistryResponse = (
+	registry: Parameters,
+): Parameters & {
+	'@ts': string // e.g. '2024-03-06T14:42:12.690Z'
+	'@version': 1
+	'@context': 'https://github.com/hello-nrfcloud/public-parameter-registry-aws-js'
+} => ({
+	'@version': 1,
+	'@ts': new Date().toISOString(),
+	'@context':
+		'https://github.com/hello-nrfcloud/public-parameter-registry-aws-js',
+	...registry,
+})
+
+const sendJSON = (
+	res: ServerResponse<IncomingMessage>,
+	payload: Record<string, unknown>,
+): void => {
+	console.debug(`[backend]`, JSON.stringify(payload))
+	res.setHeader('Content-type', 'application/json; charset=utf-8')
+	res.write(JSON.stringify(payload))
+	res.end()
+}
--- e2e-tests/testDataServerPlugin.ts
+++ e2e-tests/testDataServerPlugin.ts
@@ -1,7 +1,6 @@
 import type { Plugin } from 'vite'
 import type { Parameters } from '../src/context/Parameters.js'
-import type { IncomingMessage, ServerResponse } from 'http'
-import { Context } from '@hello.nrfcloud.com/proto/hello'
+import { mockBackend } from './mock-backend.ts'
 
 export const testdataServerPlugin = ({
 	registry,
@@ -10,47 +9,8 @@ export const testdataServerPlugin = ({
 }): Plugin => ({
 	name: 'testdata-server',
 	configureServer: (server) => {
-		server.middlewares.use((req, res, next) => {
-			if ((req.url?.startsWith('/e2e/') ?? false) === false) return next()
-			if (req.url === '/e2e/registry.json') {
-				return sendJSON(res, generateRegistryResponse(registry))
-			}
-			if (req.url === '/e2e/api/devices') {
-				return sendJSON(res, {
-					'@context': Context.map.devices,
-					devices: [],
-				})
-			}
-			if (req.url === '/e2e/api/thingyWorldShadows') {
-				return sendJSON(res, {
-					'@context': Context.map.devices,
-					devices: [],
-				})
-			}
-			return next()
-		})
+		Object.entries(mockBackend({ registry })).forEach(([route, handler]) =>
+			server.middlewares.use(route, handler),
+		)
 	},
 })
-
-const generateRegistryResponse = (
-	registry: Parameters,
-): Parameters & {
-	'@ts': string // e.g. '2024-03-06T14:42:12.690Z'
-	'@version': 1
-	'@context': 'https://github.com/hello-nrfcloud/public-parameter-registry-aws-js'
-} => ({
-	'@version': 1,
-	'@ts': new Date().toISOString(),
-	'@context':
-		'https://github.com/hello-nrfcloud/public-parameter-registry-aws-js',
-	...registry,
-})
-
-const sendJSON = (
-	res: ServerResponse<IncomingMessage>,
-	payload: Record<string, unknown>,
-): void => {
-	res.setHeader('Content-type', 'application/json; charset=utf-8')
-	res.write(JSON.stringify(payload))
-	res.end()
-}
