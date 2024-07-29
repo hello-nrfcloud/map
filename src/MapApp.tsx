706bc406d18e64b4c8fcce2e00425961b0ef0b21
Markus Tacker <m@coderbyheart.com>
2024-07-29 14:03:45 +0200

    fix: set app height based on viewport
    The CSS 100vh does not work well in mobile browsers

--- src/MapApp.tsx
+++ src/MapApp.tsx
@@ -9,25 +9,35 @@ import { SidebarNav } from './component/SidebarNav.tsx'
 import { Tutorial } from './component/Tutorial/Tutorial.tsx'
 import { Sidebar as ViewSourceSidebar } from './component/ViewSource.js'
 import { WIPSidebar } from './component/WIPSidebar.tsx'
+import { TutorialHighlight } from './component/Tutorial/TutorialHighlight.tsx'
+import { createEffect } from 'solid-js'
 
 import './Layout.css'
-import { TutorialHighlight } from './component/Tutorial/TutorialHighlight.tsx'
 
-export const MapApp = () => (
-	<div id="layout">
-		<APIHealth />
-		<UpdateAvailable />
-		<NordicHeader />
-		<SidebarNav />
-		<div id="sidebar">
-			<Tutorial />
-			<WIPSidebar />
-			<DeviceSidebar />
-			<SearchSidebar />
-			<ViewSourceSidebar />
-			<AddDeviceSidebar />
+export const MapApp = () => {
+	createEffect(() => {
+		// In mobile browsers 100vh doesn't take into account the address bar.
+		// However, this app's sidebar should not overflow the viewport.
+		const appHeight = `${window.innerHeight}px`
+		document.documentElement.style.setProperty('--app-height', appHeight)
+		console.debug(`[MapApp]`, 'appHeight:', appHeight)
+	})
+	return (
+		<div id="layout">
+			<APIHealth />
+			<UpdateAvailable />
+			<NordicHeader />
+			<SidebarNav />
+			<div id="sidebar">
+				<Tutorial />
+				<WIPSidebar />
+				<DeviceSidebar />
+				<SearchSidebar />
+				<ViewSourceSidebar />
+				<AddDeviceSidebar />
+			</div>
+			<AllDevicesMap />
+			<TutorialHighlight />
 		</div>
-		<AllDevicesMap />
-		<TutorialHighlight />
-	</div>
-)
+	)
+}
